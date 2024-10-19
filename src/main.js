fa2439f50596639cd8b8d3a9f01c25bb6efb3776
Raflirr70 <rafliradiansyah70@gmail.com>
2024-10-19 22:53:51 +0700

    berhasil cuy

--- src/main.js
+++ src/main.js
@@ -10,13 +10,15 @@ btnTambah.onclick = (e) => {
    }
    let task = document.createElement("div");
    task.id = `${Date.now()}`;
-   task.className = `...`;
+   task.className = `flex flex-wrap basis-full`;
    task.innerHTML = `
-         <p class="...">${title}</p>
-         <input type="button" id="btn-selesai" value="SELESAI" data-task="${task.id}"
-                class="...">
-         <input type="button" id="btn-hapus" value="HAPUS" data-task="${task.id}"
-                class="...">
+      <div class="flex mb-5">
+         <span class="text-gray-700 pl-5 font-bold w-72">${title}</span>
+         <div class="flex space-x-2">
+            <input type="button" id="btn-selesai" value="SELESAI" data-task="${task.id}" class="bg-green-500 text-white px-3 py-1 rounded-md mr-5">
+            <input type="button" id="btn-hapus" value="HAPUS" data-task="${task.id}" class="bg-red-500 text-white px-3 py-1 rounded-md">
+         </div>
+      </div>
    `;
    output.appendChild(task);
    input.value = "";
